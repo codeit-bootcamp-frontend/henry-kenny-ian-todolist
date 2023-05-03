c3d299b7231d57050d763c7e164749b932ca2caa
SeyoungCho <wnl383@naver.com>
2023-05-04 02:18:43 +0900

    feat: 프로그레스바 컴포넌트에 애니메이션 추가

--- src/Components/ProgressBar/ProgressBar.jsx
+++ src/Components/ProgressBar/ProgressBar.jsx
@@ -5,11 +5,13 @@ const PROGRESS_CONTAINER = {
   height: "20px",
   borderRadius: "10px",
   margin: "50px auto 40px",
+  overflow: "hidden",
 };
 const PROGRESS_STATUS = {
   background: "linear-gradient(90deg, #007A31 -6.7%, #23FB00 95%)",
   height: "100%",
   borderRadius: "10px",
+  transition: "width 1s cubic-bezier(0, 0.1, 0, 1.0)",
 };
 
 const ProgressBar = ({ progress }) => {
