b2e4fad334af2544d8c5dbcfee1c974ad133b740
SeyoungCho <wnl383@naver.com>
2023-05-06 23:56:14 +0900

    feat: 투두아이템 추가시 빈 텍스트 체크 로직 및 애니메이션 추가

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useState } from "react";
+import React, { useContext, useRef, useState, useEffect } from "react";
 import { ThemeContext } from "../../Contexts/ThemeContext";
 import ModalPortal from "./ModalPortal";
 import Button from "../Button/Button";
@@ -19,10 +19,14 @@ const Modal = ({ onClose, userInfo, todoItem }) => {
   const titleRef = useRef(null);
   const queryPath = `/users/${userInfo?.uid}/todos`;
   const [title, setTitle] = useState(todoItem ? todoItem.title : "");
+  const [warning, setWarning] = useState(false);
+  let warningClass = warning ? "warning" : "";
+
   const handleChange = () => {
     if (!titleRef.current) return;
     setTitle(titleRef.current.value);
   };
+
   const toastPopUpAnimation = {
     initial: {
       y: 200,
@@ -37,31 +41,56 @@ const Modal = ({ onClose, userInfo, todoItem }) => {
       opacity: 0,
     },
   };
+
+  const validateForm = (e) => {
+    e.preventDefault();
+    if (titleRef.current.value === "") {
+      setWarning(true);
+      setTimeout(() => {
+        setWarning(false);
+      }, 400);
+      return;
+    }
+    handleSubmit(e);
+    onClose();
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     if (todoItem) {
-      // 수정 용도로 사용하는 경우 submit 함수
-      const q = query(
-        collection(firestore, queryPath),
-        where("id", "==", todoItem.id)
-      );
-      const querySnapshot = await getDocs(q);
-
-      querySnapshot.forEach(async (doc) => {
-        await updateDoc(doc.ref, {
-          title,
-        });
-      });
-    } else {
-      // 추가 용도로 사용하는 경우 submit 함수
-      await addDoc(collection(firestore, `users/${userInfo.uid}/todos`), {
-        title,
-        isComplete: false,
-        id: new Date().getTime(),
-      });
+      editItem();
+      return;
     }
+    addItem();
   };
+
+  const editItem = async () => {
+    // 수정 용도로 사용하는 경우 submit 함수
+    const q = query(
+      collection(firestore, queryPath),
+      where("id", "==", todoItem.id)
+    );
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach(async (doc) => {
+      await updateDoc(doc.ref, {
+        title,
+      });
+    });
+  };
+
+  const addItem = async () => {
+    // 추가 용도로 사용하는 경우 submit 함수
+    await addDoc(collection(firestore, `users/${userInfo.uid}/todos`), {
+      title,
+      isComplete: false,
+      id: new Date().getTime(),
+    });
+  };
+
+  useEffect(() => {
+    if (warning) warningClass = "warning";
+  }, [warning]);
 
   return (
     <ModalPortal>
@@ -71,7 +100,7 @@ const Modal = ({ onClose, userInfo, todoItem }) => {
           initial={"initial"}
           animate={"animate"}
           exit={"exit"}
-          className={`${styles.wrapper} ${theme} modal-box-${theme}`}
+          className={`${styles.wrapper} ${theme} modal-box-${theme} ${styles[warningClass]}`}
           onClick={(e) => {
             e.stopPropagation();
           }}
@@ -80,20 +109,16 @@ const Modal = ({ onClose, userInfo, todoItem }) => {
             <textarea
               className={`${styles.input} ${theme} concave-${theme}-sm`}
               type="text"
-              placeholder="내용을 입력하세요..."
+              placeholder={
+                warning ? "내용을 입력하라고요!!" : "내용을 입력하세요..."
+              }
               value={title}
               ref={titleRef}
               onChange={handleChange}
             />
             <div className={styles.buttonsContainer}>
               <Button buttonType="close" onClick={onClose} />
-              <Button
-                buttonType="confirm"
-                onClick={(e) => {
-                  handleSubmit(e);
-                  onClose();
-                }}
-              />
+              <Button buttonType="confirm" onClick={validateForm} />
             </div>
           </form>
         </motion.div>
